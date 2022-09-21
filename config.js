2e01864008565b45b6027d620a052fb5e4290ad8
SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>
2022-09-22 07:30:36 +0800

    Update config.js
    Signed-off-by: SxyoBotz1 <112947047+SxyoBotz1@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['6287793236775','']
+global.premium = ['6287793236775','6282147562437']
+global.botname = 'SxyoBotz-MD'
+global.ownername = 'Aji'
+global.group1 = 'https://chat.whatsapp.com/JS11vcFkCpM93SycO5Z9tI'
+global.group2 = 'https://chat.whatsapp.com/FsJO4C4bLze8LxujNSAzRS'
+global.packname = 'SxyoBotz-MD'
+global.ytchannel = 'https://youtu.be/ww4z2m3uORE'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
@@ -65,7 +65,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'Aji'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
